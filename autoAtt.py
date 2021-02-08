c3dd8a47f2c314b330d2e4bf32235c3ae82c1f02
dpaceoffice <dpaceoffice@gmail.com>
2021-02-08 10:11:05 -0600

    main script

--- autoAtt.py
+++ autoAtt.py
@@ -34,3 +34,80 @@ driver = webdriver.Firefox(executable_path='drivers\geckodriver.exe')
 #driver = webdriver.Ie(executable_path='drivers\geckodriver.exe')
 
 #########################################################################
+
+#Waits 10 seconds for page to load before timing out, may need to be longer for slower internet
+driver.implicitly_wait(timeout)
+
+def checkInfo():
+    if(email == '' or password == ''):
+        return False
+    return True
+
+def init():
+    driver.get('https://moodle.uno.edu/mod/attendance/view.php?id=1283452')
+    sign_box = driver.find_element_by_link_text('UNO Sign In')
+    sign_box.click()
+
+def log_email(username):
+    email_field = driver.find_element_by_id('i0116')
+    email_field.send_keys(''+username+'' + Keys.RETURN)
+
+def log_pass(password):
+    password_field = driver.find_element_by_id('i0118')
+    signIn_button = driver.find_element_by_id('idSIButton9')
+    password_field.send_keys(''+password)
+    signIn_button.click()
+
+def att_gateway():
+    try:
+        att_button = driver.find_element_by_partial_link_text('Submit attendance')
+        att_button.click()
+    except NoSuchElementException:
+        print('Attendance not avaliable or the username/password may be wrong')
+        print('Closing in 5 seconds...')
+        driver.quit()
+        time.sleep(5)
+        sys.exit()
+
+def mark_attendence():
+    present_button = driver.find_element_by_id('id_status_9601')
+    save_button = driver.find_element_by_id('id_submitbutton')
+    present_button.click()
+    save_button.click()
+
+def startup(username, password):
+    init()
+    log_email(username)
+    time.sleep(2)#necessary
+    log_pass(password)
+    att_gateway()
+    mark_attendence()
+    driver.quit()
+    return True
+
+#Python cant automatically start the application through the browser for obvious reasons
+def start_zoom():
+    driver.get('https://uno.zoom.us/j/86818604192?pwd=c3FpSXR6a3FPSENKVTUyNFdSWmFsUT09#success')
+    #driver.get('https://uno.zoom.us/j/86818604192?pwd=c3FpSXR6a3FPSENKVTUyNFdSWmFsUT09')
+    launch_meeting = driver.find_element_by_class_name('_1FvRrPS6')
+    launch_meeting.click()
+
+def main():
+    if(startup(email, password)):
+        print('Sign in successful.')
+    if(using_zoom):
+        start_zoom()
+        print('Zoom started - script will automatically self-destruct in 15 seconds.')
+        time.sleep(15)
+        driver.quit()
+        sys.exit
+
+if(__name__ == "__main__"):
+    if(checkInfo() == False):
+        print('Moodle email and password must be configured to run this script directly.')
+        print('Closing in 5 seconds...')
+        driver.close()
+        time.sleep(5)
+        sys.exit
+    else:
+        main()
