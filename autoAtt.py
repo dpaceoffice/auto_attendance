b27b9aae20534f2e784b9dac265cd14fbf0ac5e3
dpaceoffice <dpaceoffice@gmail.com>
2021-02-08 10:10:26 -0600

    Script Info

---
+++ autoAtt.py
@@ -0,0 +1,9 @@
+"""
+Author David Pace
+
+Purpose:
+This script is designed to automatically take roll on moodle for software engineering, and start zoom after. 
+The configuations must be set below or adjusted to your system to properly work. I.E timeout may need to be extended for slower internet, or lowered for faster internet
+The driver must also be set to use the broswer installed on your system. This script has yet to go through rigourus testing so if any bugs may be encounted please report those.  
+
+"""
